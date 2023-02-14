ed525f83f5158cfb995ec20a7b8f6a435b5f60d6
Ruben <rubdeivis@gmail.com>
2023-02-14 12:06:07 -0400

    move languages to constants
    Signed-off-by: Ruben <rubdeivis@gmail.com>

--- src/storage/entities/settings/LanguageSetting.ts
+++ src/storage/entities/settings/LanguageSetting.ts
@@ -1,15 +1,12 @@
+import { LANGUAGES } from "@src/utils/constants";
 import Setting from "./Setting";
 
 export default class LanguageSetting extends Setting {
   static getDefault() {
-    return { lang: "en", name: "English", englishName: "English" };
+    return LANGUAGES[0];
   }
 
   static getSupportedLanguages() {
-    return [
-      { lang: "en", name: "English", englishName: "English" },
-      { lang: "es", name: "Español", englishName: "Spanish" },
-      { lang: "jp", name: "日本語", englishName: "Japanese" },
-    ];
+    return LANGUAGES;
   }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,9 @@ export const NETWORK = "network";
 export const BACKUP = "backup";
 
 export const ACCOUNT_PATH = `m/44'/60'/0'/0/0`;
+
+export const LANGUAGES = [
+  { lang: "en", name: "English", englishName: "English" },
+  { lang: "es", name: "Español", englishName: "Spanish" },
+  { lang: "jp", name: "日本語", englishName: "Japanese" },
+];
