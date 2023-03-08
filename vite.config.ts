533ba1c8ec6b1de30c51e4d5c3ff10bbc7b43857
Jose Ramirez <jarcodallo@gmail.com>
2023-03-08 07:04:57 -0400

    treat the used modules separately on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -83,6 +83,11 @@ export default defineConfig({
       },
       output: {
         entryFileNames: (chunk) => `src/entries/${chunk.name}/index.js`,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
       },
       plugins: [rollupNodePolyFill()],
     },
