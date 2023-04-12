161e8c21b165a31c735357d31688d38fda4e2082
Ruben <rubdeivis@gmail.com>
2023-04-12 16:32:27 -0400

    fix build
    Signed-off-by: Ruben <rubdeivis@gmail.com>

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,4 @@
-export const isChrome = process.env.BROWSER_TARGET === "chrome";
+export const isChrome = process.env.BROWSER_TARGET === "CHROME";
 export const isProduction = process.env.NODE_ENV === "production";
 
 export const getWebAPI: () => typeof window.chrome = () =>
