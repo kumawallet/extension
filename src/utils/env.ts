6494f14ab4d3d624200ba2f088331c2f5a39d803
Fernando Sirni <fsirni@gmail.com>
2023-05-31 12:09:16 -0300

    add version, about us and bug report

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,6 @@
 export const isChrome = process.env.BROWSER_TARGET === "CHROME";
 export const isProduction = process.env.NODE_ENV === "production";
+export const version = process.env.KUMA_VERSION || "0.0.0";
 
 export const getWebAPI: () => typeof window.chrome = () =>
   navigator.userAgent.match(/chrome|chromium|crios/i) ? chrome : window.browser;
