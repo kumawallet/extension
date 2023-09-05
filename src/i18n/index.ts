157b724ba3613b2081cb8f212cdd977a957c9acf
kushynoda <egemenutku.kus@ogr.deu.edu.tr>
2023-09-05 21:40:04 +0300

    A Turkish language file has been added. Additionally, hardcoded values in some files have been made compatible with translations, and incorrect values have been corrected.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,5 +2,6 @@ import en from "./en.json";
 import es from "./es.json";
 import jp from "./jp.json";
 import it from "./it.json";
+import tr from "./tr.json"
 
-export { en, es, jp, it };
+export {en, es, jp, it, tr};
