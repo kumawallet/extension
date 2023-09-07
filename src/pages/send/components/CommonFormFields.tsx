d62964f3e3f435282182c354c9dd492dc207e6b0
Egemen Kuş <47275560+egemenkus@users.noreply.github.com>
2023-09-07 18:44:36 +0300

    A Turkish language file has been added. Additionally, hardcoded values in some files have been made compatible with translations, and incorrect values have been corrected. (#39)

--- src/pages/send/components/CommonFormFields.tsx
+++ src/pages/send/components/CommonFormFields.tsx
@@ -1,111 +1,112 @@
-import { InputErrorMessage } from "@src/components/common";
-import { useFormContext } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import { TbChevronRight } from "react-icons/tb";
-import { NumericFormat } from "react-number-format";
-import { Destination } from "./Destination";
-import { SelectableAsset } from "./SelectableAsset";
-import { SelectableChain } from "./SelectableChain";
-import { useNetworkContext } from "@src/providers";
-import { useEffect, useState } from "react";
+import {InputErrorMessage} from "@src/components/common";
+import {useFormContext} from "react-hook-form";
+import {useTranslation} from "react-i18next";
+import {TbChevronRight} from "react-icons/tb";
+import {NumericFormat} from "react-number-format";
+import {Destination} from "./Destination";
+import {SelectableAsset} from "./SelectableAsset";
+import {SelectableChain} from "./SelectableChain";
+import {useNetworkContext} from "@src/providers";
+import {useEffect, useState} from "react";
 import Extension from "@src/Extension";
-import { Chain } from "@src/storage/entities/Chains";
-import { XCMAlertMessage } from "./XCMAlertMessage";
+import {Chain} from "@src/storage/entities/Chains";
+import {XCMAlertMessage} from "./XCMAlertMessage";
 
 export const CommonFormFields = () => {
-  const { t } = useTranslation("send");
+    const {t} = useTranslation("send");
 
-  const {
-    state: { selectedChain },
-  } = useNetworkContext();
+    const {
+        state: {selectedChain},
+    } = useNetworkContext();
 
-  const {
-    setValue,
-    getValues,
-    watch,
-    formState: { errors },
-  } = useFormContext();
+    const {
+        setValue,
+        getValues,
+        watch,
+        formState: {errors},
+    } = useFormContext();
 
-  const to = watch("to");
+    const to = watch("to");
 
-  const [destinationChains, setDestinationChains] = useState<Chain[]>([]);
+    const [destinationChains, setDestinationChains] = useState<Chain[]>([]);
 
-  const getDestinationChains = async () => {
-    let chains = [selectedChain];
+    const getDestinationChains = async () => {
+        let chains = [selectedChain];
 
-    if (selectedChain.xcm) {
-      const xcmChains = await Extension.getXCMChains(selectedChain.name);
-      chains = [...chains, ...xcmChains];
-    }
+        if (selectedChain.xcm) {
+            const xcmChains = await Extension.getXCMChains(selectedChain.name);
+            chains = [...chains, ...xcmChains];
+        }
 
-    setDestinationChains(chains);
-  };
+        setDestinationChains(chains);
+    };
 
-  useEffect(() => {
-    if (selectedChain.name) {
-      getDestinationChains();
-    }
-  }, [selectedChain]);
+    useEffect(() => {
+        if (selectedChain.name) {
+            getDestinationChains();
+        }
+    }, [selectedChain]);
 
-  useEffect(() => {
-    setValue("isXcm", to.name !== selectedChain.name);
-  }, [to]);
+    useEffect(() => {
+        setValue("isXcm", to.name !== selectedChain.name);
+    }, [to]);
 
-  return (
-    <>
-      <div className="mx-auto">
-        <div className="flex gap-2 justify-center items-end mb-4">
-          <div className="px-2">
-            <p className="mb-2 font-inter font-light">From:</p>
-            <SelectableChain selectedChain={getValues("from") || {}} />
-          </div>
-          <TbChevronRight size={26} className="mb-2" />
-          <div className="px-2">
-            <p className="mb-2 font-inter font-light">To:</p>
-            <SelectableChain
-              canSelectChain={true}
-              selectedChain={getValues("to")}
-              optionChains={destinationChains}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 mb-3">
-          <div>
-            <p>{t("destination_account")}</p>
-            <Destination />
-            <InputErrorMessage
-              message={errors.destinationAccount?.message as string}
-            />
-          </div>
-          <div>
-            <div className="flex gap-1 items-center">
-              <p>{t("amount")}</p>
-              <XCMAlertMessage />
+    return (
+        <>
+            <div className="mx-auto">
+                <div className="flex gap-2 justify-center items-end mb-4">
+                    <div className="px-2">
+                        <p className="mb-2 font-inter font-light">{t("from")}:</p>
+                        <SelectableChain selectedChain={getValues("from") || {}}/>
+                    </div>
+                    <TbChevronRight size={26} className="mb-2"/>
+                    <div className="px-2">
+                        <p className="mb-2 font-inter font-light">{t("to")}:</p>
+                        <SelectableChain
+                            canSelectChain={true}
+                            selectedChain={getValues("to")}
+                            optionChains={destinationChains}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4 mb-3">
+                    <div>
+                        <p>{t("destination_account")}</p>
+                        <Destination/>
+                        <InputErrorMessage
+                            message={errors.destinationAccount?.message as string}
+                        />
+                    </div>
+                    <div>
+                        <div className="flex gap-1 items-center">
+                            <p>{t("amount")}</p>
+                            <XCMAlertMessage/>
+                        </div>
+                        <div
+                            className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 flex w-full p-2.5 bg-[#343A40] border-gray-600 placeholder-gray-400 text-white">
+                            <NumericFormat
+                                className="bg-transparent w-7/12 outline-0 text-xl font-inter font-bold outline-none"
+                                allowNegative={false}
+                                allowLeadingZeros={false}
+                                value={getValues("amount")}
+                                onValueChange={({value}) => {
+                                    setValue("amount", value || "0");
+                                }}
+                                allowedDecimalSeparators={["%"]}
+                            />
+
+                            <div className="w-5/12">
+                                <SelectableAsset
+                                    onChangeAsset={(asset) => {
+                                        setValue("asset", asset);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <InputErrorMessage message={errors.amount?.message as string}/>
+                    </div>
+                </div>
             </div>
-            <div className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 flex w-full p-2.5 bg-[#343A40] border-gray-600 placeholder-gray-400 text-white">
-              <NumericFormat
-                className="bg-transparent w-7/12 outline-0 text-xl font-inter font-bold outline-none"
-                allowNegative={false}
-                allowLeadingZeros={false}
-                value={getValues("amount")}
-                onValueChange={({ value }) => {
-                  setValue("amount", value || "0");
-                }}
-                allowedDecimalSeparators={["%"]}
-              />
-
-              <div className="w-5/12">
-                <SelectableAsset
-                  onChangeAsset={(asset) => {
-                    setValue("asset", asset);
-                  }}
-                />
-              </div>
-            </div>
-            <InputErrorMessage message={errors.amount?.message as string} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
