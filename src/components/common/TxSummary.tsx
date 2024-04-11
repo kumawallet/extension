0e15d0d2dc9ea85ffe1c1e45613c35f598d592a0
Ruben <rubdeivis@gmail.com>
2024-04-11 10:30:32 -0400

    change tx confirm for swap
    Signed-off-by: Ruben <rubdeivis@gmail.com>

--- src/components/common/TxSummary.tsx
+++ src/components/common/TxSummary.tsx
@@ -1,5 +1,4 @@
 import { FC, Fragment } from 'react'
-import { useTranslation } from 'react-i18next'
 
 interface TxSummaryProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,16 +8,15 @@ interface TxSummaryProps {
 export const TxSummary: FC<TxSummaryProps> = ({
   tx
 }) => {
-  const { t } = useTranslation('send')
 
 
   return (
-    <div className='grid grid-cols-[34%_66%] gap-y-5'>
+    <div className='grid grid-cols-[34%_66%] gap-y-5 h-fit'>
       {
         Object.entries(tx).map(([key, value]) => (
           <Fragment key={key}>
-            <div className='text-sm'>{t(key)}</div>
-            <div className='text-sm text-[#9CA3AF]'>{value}</div>
+            <div className='text-sm'>{key}</div>
+            <div className='text-sm text-[#9CA3AF] overflow-auto text-ellipsis'>{value}</div>
           </Fragment>
         ))
       }
